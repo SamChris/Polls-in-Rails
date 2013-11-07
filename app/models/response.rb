210cb7eb56cea6322ae4ff583176c418d42ac472
App Academy Student <student@appacademy.io>
2013-11-07 11:51:46 -0500

    Validate one user response per question

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,6 +3,7 @@ class Response < ActiveRecord::Base
   attr_accessible :answer_choice_id, :respondent_id
 
   validates :answer_choice_id, :respondent_id, presence: true
+  validate :respondent_has_not_already_answered_question
 
   belongs_to(
     :answer_choice,
@@ -18,5 +19,42 @@ class Response < ActiveRecord::Base
     primary_key: :id
   )
 
+  def respondent_has_not_already_answered_question
+    if (existing_responses.count == 1 &&
+            existing_responses.first.respondent_id == self.respondent_id)
+      errors[:respondent_id] << "can only have one response per question"
+      return false
+    end
+    true
+  end
+
+  #private
+
+  def existing_responses
+    # part a
+    question_id = self.answer_choice.question.id
+
+    # Response.find_by_sql(<<-SQL, self.id)
+#     SELECT questions.id
+#     FROM questions
+#     JOIN answer_choices ON answer_choices.question_id = questions.id
+#     JOIN responses ON responses.answer_choice_id = answer_choices.id
+#     WHERE responses.id = ?
+
+    # part b
+
+    query = "SELECT responses.*
+      FROM responses
+      JOIN users ON users.id = responses.respondent_id
+      WHERE responses.respondent_id = ?
+      AND responses.answer_choice_id IN
+        (SELECT answer_choices.id
+         FROM answer_choices
+         JOIN questions ON questions.id = answer_choices.question_id
+         WHERE questions.id = ?)"
+
+     Response.find_by_sql([query, self.respondent_id, question_id ])
+
+  end
 
 end

2a7a408007cdfa84e1e22eb96afeef89e4e00dca
App Academy Student <student@appacademy.io>
2013-11-07 09:01:00 -0500

    Create Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polls::Application.config.secret_token = '9c7fd09ca12168f0a6c83bc7b6b2a5963477e4952259319bdd3583dca87d473b874e1eb4d5188992a25d1fd7a1c098145990566172440c4f14b8f35b74f739ec'
